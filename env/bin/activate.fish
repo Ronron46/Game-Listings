1e94ca83596599fbe950966a94e1a24361bef032
Ronron <ronflettemaxime@gmail.com>
2024-10-07 10:30:15 +0200

    fix: deleted useless tag that blocked the url on 'games' page

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ronron/Projets/liste jeux/env"
+set -gx VIRTUAL_ENV "/home/ronron/projets/Game-Listings/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
